d5cb0e6b6003fe1b6d11e7c1a07577609e3a3681
Ilya Shulzhenko <Illlya1@yandex.ru>
2020-03-24 22:28:18 +0300

    [os-2]

---
+++ app/pages/machinery/component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './index.scss';
+
+
+class MachineryPage extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+   
+    render() {
+       
+        return (            
+                <div>
+                    
+                </div>            
+        );
+    }
+}
+
+export default MachineryPage
